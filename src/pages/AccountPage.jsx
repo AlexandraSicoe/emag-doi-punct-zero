084b479ad76a2e3f3f7df3697e75876d6120d920
Lex <sicoealexandra97@gmail.com>
2023-08-30 23:47:43 +0300

    updated Account Page

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,5 +1,14 @@
-import { Grid, Typography, Box } from "@mui/joy";
+import {
+  Grid,
+  Typography,
+  Box,
+  ListItemContent,
+  ListItemButton,
+  ListItemDecorator,
+  List,
+} from "@mui/joy";
 import Navbar from "../components/Navbar";
+import ListItem from "@mui/joy/ListItem";
 
 const AccountPage = () => {
   return (
@@ -12,6 +21,7 @@ const AccountPage = () => {
           display="flex"
           alignItems="start"
           sx={{
+            flexDirection: "column",
             borderTopLeftRadius: "16px",
             borderTopRightRadius: "16px",
             backgroundColor: "white",
@@ -25,12 +35,34 @@ const AccountPage = () => {
                 "radial-gradient(circle at 12.3% 19.3%, rgb(85, 88, 218) 0%, rgb(95, 209, 249) 100.2%);",
               borderTopLeftRadius: "12px",
               borderTopRightRadius: "12px",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
             }}
             xs={12}
             md={6}
           >
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              alt="profile picture"
+              style={{
+                borderRadius: "50%",
+                height: "75px",
+                width: "75px",
+                objectFit: "cover",
+                marginRight: "20px",
+              }}
+            />
             <Typography level="h4">Anca Chelaru</Typography>
           </Grid>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemContent>Home</ListItemContent>
+                <ListItemContent>Home</ListItemContent>
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Grid>
         <Grid
           m={3}
