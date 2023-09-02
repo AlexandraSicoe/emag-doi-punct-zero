ed8bc17863900fe94a7da33dcab17aaaa675aaa6
Lex <sicoealexandra97@gmail.com>
2023-09-02 16:01:04 +0300

    created account button for the form added functionality to the log out button with local storage

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -22,13 +22,21 @@ import VpnKeyIcon from "@mui/icons-material/VpnKey";
 import Select from "@mui/joy/Select";
 import Option from "@mui/joy/Option";
 import EmailIcon from "@mui/icons-material/Email";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AccountPage = () => {
+  const navigate = useNavigate();
+  const [isFormVisible, setIsFormVisible] = useState(false);
   const [selectedValue, setSelectedValue] = React.useState("a");
-
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
   };
+  useEffect(() => {
+    let lsUser = localStorage.getItem("user");
+    lsUser = JSON.parse(lsUser);
+  }, []);
   return (
     <>
       <Navbar />
@@ -97,11 +105,14 @@ const AccountPage = () => {
               </ListItemButton>
             </ListItem>
             <ListItem>
-              <ListItemButton variant="plain">
-                <ListItemDecorator>
-                  <PersonIcon />
-                </ListItemDecorator>
-                Account
+              <ListItemDecorator>
+                <PersonIcon />
+              </ListItemDecorator>
+              <ListItemButton
+                variant="plain"
+                onClick={() => setIsFormVisible(!isFormVisible)}
+              >
+                {isFormVisible ? "Account" : " Account"}
               </ListItemButton>
             </ListItem>
             <ListItem>
@@ -121,7 +132,13 @@ const AccountPage = () => {
               </ListItemButton>
             </ListItem>
             <ListItem>
-              <ListItemButton variant="plain">
+              <ListItemButton
+                variant="plain"
+                onClick={() => {
+                  localStorage.setItem("user", "[]");
+                  navigate("/administrare");
+                }}
+              >
                 <ListItemDecorator>
                   <LogoutIcon />
                 </ListItemDecorator>
@@ -193,100 +210,102 @@ const AccountPage = () => {
               </Box>
             </Box>
           </Grid>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              backgroundColor: "white",
-              justifyContent: { xs: "center", md: "end" },
-              marginTop: { xs: "10px", md: "40px" },
-              paddingBottom: "25px",
-              paddingLeft: "10px",
-              paddingRight: "10px",
-              width: {
-                xs: "350px",
-                sm: "345px",
-                md: "500px",
-                borderRadius: "16px",
-              },
-            }}
-          >
-            <Typography level="h3" mb={2}>
-              Administrare date
-            </Typography>
-            <form>
-              <Box display="flex" justifyContent="space-between" mb={2}>
-                <Radio
-                  checked={selectedValue === "a"}
-                  onChange={handleChange}
-                  value="a"
-                  label="Dl."
-                  name="radio-buttons"
-                  slotProps={{ input: { "aria-label": "A" } }}
+          {isFormVisible && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "white",
+                justifyContent: { xs: "center", md: "end" },
+                marginTop: { xs: "10px", md: "40px" },
+                paddingBottom: "25px",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                width: {
+                  xs: "350px",
+                  sm: "345px",
+                  md: "500px",
+                  borderRadius: "16px",
+                },
+              }}
+            >
+              <Typography level="h3" mb={2}>
+                Administrare date
+              </Typography>
+              <form>
+                <Box display="flex" justifyContent="space-between" mb={2}>
+                  <Radio
+                    checked={selectedValue === "a"}
+                    onChange={handleChange}
+                    value="a"
+                    label="Dl."
+                    name="radio-buttons"
+                    slotProps={{ input: { "aria-label": "A" } }}
+                  />
+                  <Radio
+                    checked={selectedValue === "b"}
+                    onChange={handleChange}
+                    value="b"
+                    label="Dna."
+                    name="radio-buttons"
+                    slotProps={{ input: { "aria-label": "B" } }}
+                  />
+                </Box>
+
+                <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+                  Nume si prenume:
+                </FormLabel>
+                <Input
+                  startDecorator={<PersonIcon />}
+                  type="text"
+                  sx={{ mb: 1 }}
                 />
-                <Radio
-                  checked={selectedValue === "b"}
-                  onChange={handleChange}
-                  value="b"
-                  label="Dna."
-                  name="radio-buttons"
-                  slotProps={{ input: { "aria-label": "B" } }}
+                <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+                  Email nou:
+                </FormLabel>
+                <Input
+                  startDecorator={<EmailIcon />}
+                  type="text"
+                  sx={{ mb: 1 }}
                 />
-              </Box>
+                <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+                  Parola noua:
+                </FormLabel>
+                <Input
+                  startDecorator={<VpnKeyIcon />}
+                  type="text"
+                  sx={{ mb: 1 }}
+                />
 
-              <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-                Nume si prenume:
-              </FormLabel>
-              <Input
-                startDecorator={<PersonIcon />}
-                type="text"
-                sx={{ mb: 1 }}
-              />
-              <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-                Email nou:
-              </FormLabel>
-              <Input
-                startDecorator={<EmailIcon />}
-                type="text"
-                sx={{ mb: 1 }}
-              />
-              <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-                Parola noua:
-              </FormLabel>
-              <Input
-                startDecorator={<VpnKeyIcon />}
-                type="text"
-                sx={{ mb: 1 }}
-              />
+                <Box
+                  backgroundColor="white"
+                  display="flex"
+                  justifyContent="space-between"
+                  mb={2}
+                >
+                  <Select sx={{ width: "100px" }} placeholder="Ziua">
+                    <Option>1</Option>
+                    <Option>2</Option>
+                    <Option>3</Option>
+                  </Select>
+                  <Select sx={{ width: "100px" }} placeholder="Luna">
+                    <Option>1</Option>
+                    <Option>2</Option>
+                    <Option>3</Option>
+                  </Select>
+                  <Select sx={{ width: "100px" }} placeholder="Anul">
+                    <Option>1990</Option>
+                    <Option>1991</Option>
+                    <Option>1992</Option>
+                  </Select>
+                </Box>
 
-              <Box
-                backgroundColor="white"
-                display="flex"
-                justifyContent="space-between"
-                mb={2}
-              >
-                <Select sx={{ width: "100px" }} placeholder="Ziua">
-                  <Option>1</Option>
-                  <Option>2</Option>
-                  <Option>3</Option>
-                </Select>
-                <Select sx={{ width: "100px" }} placeholder="Luna">
-                  <Option>1</Option>
-                  <Option>2</Option>
-                  <Option>3</Option>
-                </Select>
-                <Select sx={{ width: "100px" }} placeholder="Anul">
-                  <Option>1990</Option>
-                  <Option>1991</Option>
-                  <Option>1992</Option>
-                </Select>
-              </Box>
-
-              <Button size="lg" sx={{ textAlign: "center" }}>
-                Salveaza informatiile
-              </Button>
-            </form>
-          </Box>
+                <Button size="lg" sx={{ textAlign: "center" }}>
+                  Salveaza informatiile
+                </Button>
+              </form>
+            </Box>
+          )}
         </Grid>
       </Grid>
     </>
