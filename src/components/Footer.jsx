8cbdff3ef13b7cb6fef4be39c8e456f718215733
Lex <sicoealexandra97@gmail.com>
2023-11-03 10:03:23 +0200

    mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
       sx={{
         padding: "25px",
         display: "flex",
-        flexDirection: "row",
+        flexDirection: { xs: "column", sm: "row", md: "row" },
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: { xs: "center", md: "start" },
         backgroundImage:
           "linear-gradient(67deg, rgba(2,0,36,1) 0%, rgba(139,92,246,1) 0%, rgba(139,92,246,1) 23%, rgba(168,110,204,1) 46%, rgba(181,118,185,1) 55%, rgba(186,121,178,1) 66%, rgba(246,159,92,1) 98%)",
       }}
@@ -19,19 +19,25 @@ const Footer = () => {
           display: "flex",
           flexDirection: "column",
           width: "250px",
-          alignItems: "center",
+          color: "white",
         }}
       >
         <Typography level="h3">E20</Typography>
-        <List sx={{ color: "white" }}>
+        <List>
           <ListItem>
-            <Link to="#">Blog</Link>
+            <Link style={{ color: "white" }} to="#">
+              Blog
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/about-us">Despre noi</Link>
+            <Link style={{ color: "white" }} to="/about-us">
+              Despre noi
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/contact">Contact</Link>
+            <Link style={{ color: "white" }} to="/contact">
+              Contact
+            </Link>
           </ListItem>
         </List>
       </Box>
@@ -40,51 +46,71 @@ const Footer = () => {
 
         <List sx={{ color: "white" }}>
           <ListItem>
-            <Link to="#">Livrarea produselor</Link>
+            <Link style={{ color: "white" }} to="#">
+              Livrarea produselor
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="#">Politica de retur</Link>
+            <Link style={{ color: "white" }} to="#">
+              Politica de retur
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">ANPC</Link>
+            <Link style={{ color: "white" }} to="/#">
+              ANPC
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Formular Retur</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Formular Retur
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Info</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Info
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Cariere</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Cariere
+            </Link>
           </ListItem>
         </List>
       </Box>
       <Box sx={{ display: "flex", flexDirection: "column", width: "250px" }}>
         <Typography level="h3">UTILE</Typography>
 
-        <List sx={{ color: "white" }}>
+        <List>
           <ListItem>
             <Link style={{ color: "white" }} to="#">
               Blog
             </Link>
           </ListItem>
           <ListItem>
-            <Link to="#">Cum cumpar?</Link>
+            <Link style={{ color: "white" }} to="#">
+              Cum cumpar?
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Termeni si Conditii</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Termeni si Conditii
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">
+            <Link style={{ color: "white" }} to="/#">
               Politica de confidentialitate si protectia datelor cu caracter
               personal
             </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Info</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Info
+            </Link>
           </ListItem>
           <ListItem>
-            <Link to="/#">Cariere</Link>
+            <Link style={{ color: "white" }} to="/#">
+              Cariere
+            </Link>
           </ListItem>
         </List>
       </Box>
