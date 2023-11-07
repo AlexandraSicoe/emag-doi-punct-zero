e7051a967cce3600f028e1dbccfbcc95e8b15299
Alexandra Sicoe <sicoealexandra97@gmail.com>
2023-11-07 14:04:53 +0200

    NoOrders update

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,14 +1,14 @@
 import { Box, Grid, Typography } from "@mui/joy";
 import Input from "@mui/joy/Input";
-import Order from "../components/Order";
 import Option from "@mui/joy/Option";
 import Select from "@mui/joy/Select";
-import * as React from "react";
 import Pagination from "@mui/material/Pagination";
-import useQuery from "../helpers/useQuery";
+import axios from "axios";
+import * as React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import Order from "../components/Order";
+import useQuery from "../helpers/useQuery";
 import NoOrders from "./NoOrders";
 
 const Orders = ({ userId }) => {
@@ -163,7 +163,37 @@ const Orders = ({ userId }) => {
             </Box>
           </Box>
         </Grid>
-        <NoOrders />
+        {orders.length === 0 ? (
+          <NoOrders />
+        ) : (
+          <Grid
+            sx={{
+              padding: "25px",
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "white",
+              marginBottom: "25px",
+              borderRadius: "16px",
+              marginTop: { xs: "25px", md: "25px" },
+              marginLeft: { xs: "0px", md: "25px" },
+            }}
+          >
+            {orders.map((order, index) => (
+              <Box key={index}>
+                <Order order={order} />
+              </Box>
+            ))}
+
+            <Pagination
+              count={totalPages}
+              color="primary"
+              page={ordersPage}
+              showFirstButton
+              showLastButton
+              onChange={handleChange}
+            />
+          </Grid>
+        )}
       </Grid>
     </>
   );
