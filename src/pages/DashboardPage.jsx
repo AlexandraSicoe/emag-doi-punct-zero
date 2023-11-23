6345c792df1844c0e9b73d1023f53561ef6c8c17
Lex <sicoealexandra97@gmail.com>
2023-11-23 17:48:48 +0200

    asasa

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,7 @@
-import { Dashboard } from "@mui/icons-material";
-import { Box, Button, Grid, Typography } from "@mui/joy";
+import { Grid, Box, Typography, FormLabel, Button } from "@mui/joy";
+import Input from "@mui/joy/Input";
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
 const DashboardPage = () => {
   return (
     <>
@@ -9,21 +11,94 @@ const DashboardPage = () => {
         alignItems="center"
         flexDirection="column"
         backgroundColor="#F2F2F7"
-        p={3}
       >
         <Box
           sx={{
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            alignItems: "start",
             flexDirection: "column",
             backgroundColor: "white",
             borderRadius: "16px",
             width: { xs: "100%", sm: "100%", md: "900px" },
             marginTop: { xs: "10px", md: "25px" },
             margin: { sx: "0px", sm: "0px", md: "25px" },
+            padding: "25px",
           }}
-        ></Box>
+        >
+          <Typography level="body-lg">Adaugă un produs</Typography>
+          <Box
+            sx={{
+              width: "100%",
+              borderBottom: "1px solid black",
+              padding: "10px",
+              alignSelf: "center",
+            }}
+          ></Box>
+
+          <form style={{ paddingTop: "10px" }}>
+            <FormLabel sx={{ fontSize: "18px", marginBottom: "5px" }}>
+              Denumire produs:
+            </FormLabel>
+            <Input
+              placeholder="eg. John Doe"
+              sx={{ border: "solid 1px	#1F51FF", marginBottom: "5px" }}
+              type="text"
+            />
+            <FormLabel sx={{ fontSize: "18px", marginBottom: "5px" }}>
+              Categorie:
+            </FormLabel>
+
+            <Select
+              sx={{
+                "&:hover": {
+                  backgroundColor: "white",
+                },
+              }}
+              placeholder="Selectează categoria"
+            >
+              <Option
+                value="Îmbrăcăminte"
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "pink",
+                  },
+                }}
+              >
+                Îmbrăcăminte
+              </Option>
+              <Option
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "pink",
+                  },
+                }}
+                value="Accesorii"
+              >
+                Accesorii
+              </Option>
+              <Option
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "pink",
+                  },
+                }}
+                value="Hrană"
+              >
+                Hrană
+              </Option>
+              <Option
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "pink",
+                  },
+                }}
+                value="Îngrijire"
+              >
+                Îngrijire
+              </Option>
+            </Select>
+          </form>
+        </Box>
       </Grid>
     </>
   );
