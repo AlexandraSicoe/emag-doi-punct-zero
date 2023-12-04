c18a33869306c05016ed1b236db3b28e784e6970
Bogdan Falk <bogdan37falk@gmail.com>
2023-12-04 18:29:18 +0200

    Quick Fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -60,64 +60,123 @@ const ProductCard = ({ product, setCartData, cartData }) => {
             alt="product"
           />
         </AspectRatio>
-        <CardContent orientation="horizontal">
+        <CardContent orientation="horizontal" sx={{display:"flex",justifyContent:"space-between"}}>
           <div>
             <Typography level="body-xs">Preț:</Typography>
             <Typography fontSize="md" fontWeight="lg">
               {product.price.toFixed(2)} RON
             </Typography>
           </div>
-          <Button
-            variant="solid"
-            size="md"
-            color="primary"
-            sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-            onClick={(e) => {
-              e.stopPropagation();
-              let _product = JSON.parse(JSON.stringify(product));
-              if (cartData.some((item) => item._id === _product._id)) {
-                cartData.forEach((obj) => {
-                  if (obj._id === _product._id) {
-                    _product = JSON.parse(JSON.stringify(obj));
-                    if (/^[0-9]/.test(_product.name.charAt(0))) {
-                      let numberOfProducts = getFirstNumber(_product.name);
-                      numberOfProducts++;
 
-                      _product.price = numberOfProducts * product.price;
-                      _product.name = replaceFirstNumber(
-                        _product.name,
-                        numberOfProducts
-                      );
-                    } else {
-                      _product.name = "2x " + _product.name;
-                      _product.price = _product.price * 2;
-                    }
-                  }
-                });
-                let _cartData = JSON.parse(JSON.stringify(cartData));
-                _cartData = _cartData.map((item) => {
-                  return item._id === _product._id ? _product : item;
-                });
-                localStorage.setItem("cart", JSON.stringify(_cartData));
-
-                setCartData(_cartData);
-                window.dispatchEvent(new Event("customStorageChange"));
-              } else {
-                const _cartData = [...cartData, product];
-                localStorage.setItem("cart", JSON.stringify(_cartData));
-                setCartData(_cartData);
-                window.dispatchEvent(new Event("customStorageChange"));
-              }
+          <Box
+            sx={{
+              display: { xs: "none", md: "inline-block" },
             }}
-            startDecorator={<ShoppingCartIcon />}
           >
-            <Typography
-              color="common.white"
-              sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+            <Button
+              variant="solid"
+              size="md"
+              color="primary"
+              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
+              onClick={(e) => {
+                e.stopPropagation();
+                let _product = JSON.parse(JSON.stringify(product));
+                if (cartData.some((item) => item._id === _product._id)) {
+                  cartData.forEach((obj) => {
+                    if (obj._id === _product._id) {
+                      _product = JSON.parse(JSON.stringify(obj));
+                      if (/^[0-9]/.test(_product.name.charAt(0))) {
+                        let numberOfProducts = getFirstNumber(_product.name);
+                        numberOfProducts++;
+
+                        _product.price = numberOfProducts * product.price;
+                        _product.name = replaceFirstNumber(
+                          _product.name,
+                          numberOfProducts
+                        );
+                      } else {
+                        _product.name = "2x " + _product.name;
+                        _product.price = _product.price * 2;
+                      }
+                    }
+                  });
+                  let _cartData = JSON.parse(JSON.stringify(cartData));
+                  _cartData = _cartData.map((item) => {
+                    return item._id === _product._id ? _product : item;
+                  });
+                  localStorage.setItem("cart", JSON.stringify(_cartData));
+
+                  setCartData(_cartData);
+                  window.dispatchEvent(new Event("customStorageChange"));
+                } else {
+                  const _cartData = [...cartData, product];
+                  localStorage.setItem("cart", JSON.stringify(_cartData));
+                  setCartData(_cartData);
+                  window.dispatchEvent(new Event("customStorageChange"));
+                }
+              }}
+              startDecorator={<ShoppingCartIcon />}
             >
-              Cumpără
-            </Typography>
-          </Button>
+              <Typography
+                color="common.white"
+                sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+              >
+                Cumpără
+              </Typography>
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              display: { xs: "block", md: "none" },
+              ml:"auto"
+            }}
+          >
+            <Button
+              variant="solid"
+              size="md"
+              color="primary"
+              sx={{ fontWeight: 600 }}
+              onClick={(e) => {
+                e.stopPropagation();
+                let _product = JSON.parse(JSON.stringify(product));
+                if (cartData.some((item) => item._id === _product._id)) {
+                  cartData.forEach((obj) => {
+                    if (obj._id === _product._id) {
+                      _product = JSON.parse(JSON.stringify(obj));
+                      if (/^[0-9]/.test(_product.name.charAt(0))) {
+                        let numberOfProducts = getFirstNumber(_product.name);
+                        numberOfProducts++;
+
+                        _product.price = numberOfProducts * product.price;
+                        _product.name = replaceFirstNumber(
+                          _product.name,
+                          numberOfProducts
+                        );
+                      } else {
+                        _product.name = "2x " + _product.name;
+                        _product.price = _product.price * 2;
+                      }
+                    }
+                  });
+                  let _cartData = JSON.parse(JSON.stringify(cartData));
+                  _cartData = _cartData.map((item) => {
+                    return item._id === _product._id ? _product : item;
+                  });
+                  localStorage.setItem("cart", JSON.stringify(_cartData));
+
+                  setCartData(_cartData);
+                  window.dispatchEvent(new Event("customStorageChange"));
+                } else {
+                  const _cartData = [...cartData, product];
+                  localStorage.setItem("cart", JSON.stringify(_cartData));
+                  setCartData(_cartData);
+                  window.dispatchEvent(new Event("customStorageChange"));
+                }
+              }}
+            >
+              <ShoppingCartIcon />
+            </Button>
+          </Box>
         </CardContent>
       </Card>
     </Box>
