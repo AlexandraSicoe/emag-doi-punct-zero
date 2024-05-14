6511f9caecd1fe489ec7e0fe3be11d9b726ef98c
AlexandraSicoe <96877358+AlexandraSicoe@users.noreply.github.com>
2024-05-14 12:05:56 +0300

    (e20 78) user details edit (#19)
    * Fixed account form
    
    * Added disabled property to Salveaza imaginea button if imageFile is null

--- src/components/AccountEditForm.jsx
+++ src/components/AccountEditForm.jsx
@@ -12,23 +12,19 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import Avatar from "react-avatar-edit";
 import axios from "axios";
+import CircularProgress from "@mui/joy/CircularProgress";
 
 const AccountEditForm = () => {
   const [imageFile, setImageFile] = useState(null);
   const [phone, setPhone] = React.useState("");
-  const [selectedValue, setSelectedValue] = React.useState("a");
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
-  const placeholders = {
-    a: {
-      name: "Popescu Ioan",
-      email: "popescuioan@gmail.com",
-    },
-    b: {
-      name: "Popescu Ioana",
-      email: "popescuioana@gmail.com",
-    },
+  const [storedUserData, setStoredUserData] = useState(null);
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    setStoredUserData((prevUserData) => ({
+      ...prevUserData,
+      prefix: Number(value),
+    }));
   };
 
   const generateRandomFileName = () => {
@@ -66,7 +62,32 @@ const AccountEditForm = () => {
     // Create a File object from the Blob
     return new File([blob], filename, { type });
   };
+  const updateUser = async (userID, updatedUserData) => {
+    try {
+      const response = await axios.put(
+        "https://e20.ro/api/user/" + userID,
+        updatedUserData,
+        {
+          headers: {
+            "Content-Type": "application/json", // Specify content type as JSON
+          },
+        }
+      );
 
+      console.log("User updated successfully:", response.data);
+      localStorage.setItem("user", JSON.stringify(response.data));
+      window.location.reload();
+    } catch (error) {
+      if (error.response) {
+        console.error("Server responded with status:", error.response.status);
+        console.error("Error message:", error.response.data);
+      } else if (error.request) {
+        console.error("No response received from server:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+    }
+  };
   const handleSaveProfilePicture = async () => {
     const userData = JSON.parse(localStorage.getItem("user"));
     const formData = new FormData();
@@ -99,6 +120,8 @@ const AccountEditForm = () => {
   useEffect(() => {
     let lsUser = localStorage.getItem("user");
     lsUser = JSON.parse(lsUser);
+    lsUser.password = "";
+    setStoredUserData(lsUser);
   }, []);
   return (
     <Box
@@ -110,140 +133,191 @@ const AccountEditForm = () => {
         marginTop: "25px",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          backgroundColor: "white",
-          alignItems: "start",
-          justifyContent: { xs: "center", md: "start" },
-          marginLeft: { xs: "0px", md: "25px" },
-          padding: "25px",
-          width: "100%",
-          borderRadius: "16px",
-        }}
-      >
-        <Typography level="h3" mb={2}>
-          Administrare date
-        </Typography>
+      {storedUserData == null ? (
+        <CircularProgress />
+      ) : (
         <Box
-          sx={{ overflowX: "hidden", maxWidth: "250px", overflowY: "hidden" }}
-        >
-          <Avatar
-            width={250}
-            height={250}
-            label="Schimbă imaginea"
-            onCrop={(preview) => {
-              setImageFile(preview);
-            }}
-          />
-        </Box>
-
-        <Button
           sx={{
-            width: "250px",
-            marginBottom: "25px",
-            borderTopLeftRadius: "0px",
-            borderTopRightRadius: "0px",
-          }}
-          onClick={() => {
-            handleSaveProfilePicture();
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "white",
+            alignItems: "start",
+            justifyContent: { xs: "center", md: "start" },
+            marginLeft: { xs: "0px", md: "25px" },
+            padding: "25px",
+            width: "100%",
+            borderRadius: "16px",
           }}
         >
-          Salvează imaginea
-        </Button>
-        <form style={{ width: "100%" }}>
-          <Box display="flex" justifyContent="start" mb={2}>
-            <Radio
-              checked={selectedValue === "a"}
-              onChange={handleChange}
-              value="a"
-              label="Dl."
-              name="radio-buttons"
-              slotProps={{ input: { "aria-label": "A" } }}
-              sx={{ marginRight: "50px" }}
-            />
-            <Radio
-              checked={selectedValue === "b"}
-              onChange={handleChange}
-              value="b"
-              label="Dna."
-              name="radio-buttons"
-              slotProps={{ input: { "aria-label": "B" } }}
+          <Typography level="h3" mb={2}>
+            Administrare date
+          </Typography>
+          <Box
+            sx={{ overflowX: "hidden", maxWidth: "250px", overflowY: "hidden" }}
+          >
+            <Avatar
+              width={250}
+              height={250}
+              label="Schimbă imaginea"
+              onCrop={(preview) => {
+                setImageFile(preview);
+              }}
             />
           </Box>
 
-          <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-            Nume si prenume:
-          </FormLabel>
-          <Input
-            placeholder={placeholders[selectedValue].name}
-            startDecorator={<PersonIcon sx={{ color: "#8b5cf6" }} />}
-            type="text"
-            sx={{ mb: 1, width: "50%" }}
-          />
-          <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-            Email nou:
-          </FormLabel>
-          <Input
-            placeholder={placeholders[selectedValue].email}
-            startDecorator={<EmailIcon sx={{ color: "#8b5cf6" }} />}
-            type="text"
-            sx={{ mb: 1, width: "50%" }}
-          />
-          <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-            Parola noua:
-          </FormLabel>
-          <Input
-            placeholder={"••••••"}
-            startDecorator={<VpnKeyIcon sx={{ color: "#8b5cf6" }} />}
-            type="text"
-            sx={{ mb: 1, width: "50%" }}
-          />
-          <Box
+          <Button
             sx={{
-              display: "flex",
-              flexDirection: "column",
+              width: "250px",
+              marginBottom: "25px",
+              borderTopLeftRadius: "0px",
+              borderTopRightRadius: "0px",
             }}
+            onClick={() => {
+              handleSaveProfilePicture();
+            }}
+            disabled={imageFile === null}
           >
-            <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-              Telefon:{" "}
-            </FormLabel>
+            Salvează imaginea
+          </Button>
+          <form style={{ width: "100%" }}>
+            <Box display="flex" justifyContent="start" mb={2}>
+              <Radio
+                checked={storedUserData.prefix === 0}
+                onChange={handleChange}
+                value="0"
+                label="Dl."
+                name="radio-buttons"
+                slotProps={{ input: { "aria-label": "A" } }}
+                sx={{ marginRight: "50px" }}
+              />
+              <Radio
+                checked={storedUserData.prefix === 1}
+                onChange={handleChange}
+                value="1"
+                label="Dna."
+                name="radio-buttons"
+                slotProps={{ input: { "aria-label": "B" } }}
+              />
+            </Box>
 
-            <PhoneInput
-              defaultCountry="ro"
-              value={phone}
-              onChange={(phone) => setPhone(phone)}
-              style={{ marginBottom: "5px", width: "50%" }}
-            />
-          </Box>
-          <Box
-            backgroundColor="white"
-            display="flex"
-            justifyContent="start"
-            flexDirection="column"
-            mb={2}
-          >
             <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
-              Data de naștere:
+              Nume si prenume:
             </FormLabel>
             <Input
-              type="date"
-              slotProps={{
-                input: {
-                  min: "2018-06-07T00:00",
-                  max: maxDate18YearsAgo,
-                },
-              }}
+              startDecorator={<PersonIcon sx={{ color: "#8b5cf6" }} />}
+              type="text"
               sx={{ mb: 1, width: "50%" }}
+              value={storedUserData.name}
+              onChange={(e) => {
+                const { value } = e.target; // Get the value from the input field
+                // Update storedUserData with the new name value
+                setStoredUserData((prevUserData) => ({
+                  ...prevUserData,
+                  name: value,
+                }));
+              }}
             />
-          </Box>
+            <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+              Email nou:
+            </FormLabel>
+            <Input
+              startDecorator={<EmailIcon sx={{ color: "#8b5cf6" }} />}
+              type="email"
+              sx={{ mb: 1, width: "50%" }}
+              value={storedUserData.email}
+              onChange={(e) => {
+                const { value } = e.target; // Get the value from the input field
+                // Update storedUserData with the new name value
+                setStoredUserData((prevUserData) => ({
+                  ...prevUserData,
+                  email: value,
+                }));
+              }}
+            />
+            <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+              Parola noua:
+            </FormLabel>
+            <Input
+              placeholder={"••••••"}
+              startDecorator={<VpnKeyIcon sx={{ color: "#8b5cf6" }} />}
+              type="password"
+              sx={{ mb: 1, width: "50%" }}
+              value={storedUserData.password}
+              onChange={(e) => {
+                const { value } = e.target; // Get the value from the input field
+                // Update storedUserData with the new name value
+                setStoredUserData((prevUserData) => ({
+                  ...prevUserData,
+                  password: value,
+                }));
+              }}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+                Telefon:{" "}
+              </FormLabel>
 
-          <Button size="lg" sx={{ textAlign: "center", width: "250px" }}>
-            Salveaza informatiile
-          </Button>
-        </form>
-      </Box>
+              <PhoneInput
+                defaultCountry="ro"
+                value={storedUserData.phone}
+                onChange={(value) => {
+                  setStoredUserData((prevUserData) => ({
+                    ...prevUserData,
+                    phone: value,
+                  }));
+                }}
+                style={{ marginBottom: "5px", width: "50%" }}
+              />
+            </Box>
+            <Box
+              backgroundColor="white"
+              display="flex"
+              justifyContent="start"
+              flexDirection="column"
+              mb={2}
+            >
+              <FormLabel sx={{ fontSize: "15px", marginBottom: "5px" }}>
+                Data de naștere:
+              </FormLabel>
+              {console.log(storedUserData)}
+              <Input
+                type="date"
+                slotProps={{
+                  input: {
+                    min: "2018-06-07T00:00",
+                    max: maxDate18YearsAgo,
+                  },
+                }}
+                sx={{ mb: 1, width: "50%" }}
+                value={storedUserData.birthday.slice(0, 10)}
+                onChange={(e) => {
+                  const { value } = e.target; // Get the value from the input field
+                  // Update storedUserData with the new name value
+                  setStoredUserData((prevUserData) => ({
+                    ...prevUserData,
+                    birthday: value,
+                  }));
+                }}
+              />
+            </Box>
+
+            <Button
+              size="lg"
+              sx={{ textAlign: "center", width: "250px" }}
+              onClick={() => {
+                updateUser(storedUserData._id, storedUserData);
+              }}
+            >
+              Salveaza informatiile
+            </Button>
+          </form>
+        </Box>
+      )}
     </Box>
   );
 };
